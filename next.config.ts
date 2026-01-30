8f2bf45a029d60b625c7d998fa13bca07a1728c6
Hiroshi Murakami <piroz@users.noreply.github.com>
2026-01-31 06:25:56 +0900

    fix config

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,7 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-  },
-  distDir: "dist",
-  output: "export",
-  trailingSlash: true,
+  }
 };
 
 export default nextConfig;
